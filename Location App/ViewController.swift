f0e736c1203a8c8379d4c265c5db3dc8cb986870
D'Kairi Boone <d.boone@setonhill.edu>
2023-04-16 18:57:17 -0400

    Initial Commit

---
+++ Location App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location App
+//
+//  Created by d.boone on 4/16/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
